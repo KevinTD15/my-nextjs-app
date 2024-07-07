5885d3714ed509010dd54eebe9696f69ae7f10d9
KevinTD15 <talaveradiazkevin1999@gmail.com>
2024-07-07 12:12:44 -0500

    added img and rescale

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div className={styles.imgContainer}>
+      <Image
+        src="https://images.unsplash.com/photo-1640161704729-cbe966a08476?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+        fill
+      />
+    </div>
+  );
 }
 
 export default About;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Links from "./links/Links";
+import Link from "next/link";
 import styles from "./navbar.module.css";
 
 function Navbar() {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <Link href="/" className={styles.logo}>
+        Logo
+      </Link>
       <div>
         <Links />
       </div>
